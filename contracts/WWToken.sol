4ca5143ee4d3d900b37348e9e86d9f5fbe16ce0f
wojciechwicher <31825333+wojciechwicher@users.noreply.github.com>
2022-01-27 20:29:01 +0100

    Update WWToken.sol

--- contracts/WWToken.sol
+++ contracts/WWToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  
 contract WWToken is ERC20, Ownable {
     constructor() ERC20("Wojciech Wicher Token A", "WWTA") {
-        _mint(msg.sender, 1001 * 10 ** 18);
+        _mint(msg.sender, 1000 * 10 ** 18);
     }
 
     function mint(address account, uint256 amount) public onlyOwner {
